2fbe0a5ca943ca8081b5d62efc547cf58568fad9
Michael Matich <matich.mike@gmail.com>
2020-09-02 10:22:00 -0400

    reducers added

--- redux.js
+++ redux.js
@@ -1,5 +1,5 @@
 // redux intro using an insurance company as example
-// Action Creators - Action - 
+// Action Creators - Action - Dispatch - Reducers - State
 
 // people dropping off a from (Action Creators)
 const createPolicy = (name, amount) => {
@@ -29,4 +29,30 @@ const createClaim = (name, amountOfMoneyToCollect) => {
       amount: amountOfMoneyToCollect
     }
   };
+};
+
+//Reducers (Departments)
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if(action.type === 'CREATE_CLAIM') {
+    return [...oldListOfClaims, action.payload];
+  }
+  return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney, action) => {
+  if(action.type === 'CREATE_CLAIM') {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.amount;
+  }
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if(action.type === 'CREATE_POLICY') {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === 'DELETE_POLICY') {
+    return listOfPolicies.filter(name => name !== action.payload.name);
+  }
+  return listOfPolicies;
 };
